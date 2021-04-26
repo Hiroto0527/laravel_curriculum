c14a7a53a7faee76c7e4f82e494ab1f30c1c606f
EC2 Default User <ec2-user@ip-172-31-30-49.us-east-2.compute.internal>
2021-04-26 11:21:10 +0900

    適切なコミットコメント

---
+++ dogtype.php
@@ -0,0 +1,9 @@
+<?php
+   class  dog {
+       public function wan(){
+           echo 'wan!'.PHP_EOL;
+       }
+       $dog = new Dog();
+       $dog ->wan() ;
+   }
+?>
---
+++ function.php
@@ -0,0 +1,10 @@
+<?php
+function sum_number() {
+    $result = 0 ;
+    for ($i = 1 ; $i <= 10 ; $i++ ) {
+        $result = $result + $i;
+    }
+    echo $result.PHP_EOL;
+}
+sum_number();
+?>
